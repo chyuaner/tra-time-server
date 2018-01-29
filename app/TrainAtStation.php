58b6a2566a1ab4a061f1c311606b6f31599c1226
Yuan Chiu <chyuaner@gmail.com>
2018-01-29 18:11:36 +0800

    新增或更新班次資料

--- app/TrainAtStation.php
+++ app/TrainAtStation.php
@@ -13,5 +13,44 @@ class TrainAtStation extends Model
      */
     protected $table = 'train_at_stations';
 
+    public $incrementing = false;
+
+    /**
+     * Indicates model primary keys.
+     */
+    protected $primaryKey = ['valid_date', 'train_code', 'station_code'];
+
+    /**
+     * 新增或更新班次資料
+     * @param  string  $valid_date 適用日期
+     * @param  int     $train_code 班次編號
+     * @param  arrays  $content    內容
+     * @return boolean             有無成功
+     */
+    public static function updateOrCreateTrainAt($valid_date, $train_code, $content)
+    {
+        $count = self::where('valid_date', $valid_date)
+                     ->where('train_code', $train_code)
+                     ->count();
+
+        if($count) {
+            return self::where('valid_date', $valid_date)
+                       ->where('train_code', $train_code)
+                       ->update($content);
+        }
+        else
+        {
+            $attr = [
+                'valid_date' => $valid_date,
+                'train_code' => $train_code
+            ];
+
+            unset($content['valid_date']);
+            unset($content['train_code']);
+            $attr = $attr + $content;
+
+            return self::create($attr);
+        }
+    }
 
 }
